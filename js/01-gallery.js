dcb2a7dd35da3587739c8eb8f103c8b988c50778
SergeZed <75592154+SergeZed@users.noreply.github.com>
2022-06-02 00:42:56 +0700

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -69,4 +69,17 @@ function onImagesContainerElClick(evt) {
   //   	`
   //     )
   //     .show();
+
+  document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+      isEscape = evt.key === "Escape" || evt.key === "Esc";
+    } else {
+      isEscape = evt.keyCode === 27;
+    }
+    if (isEscape && instance.visible) {
+      instance.close();
+    }
+  };
 }
